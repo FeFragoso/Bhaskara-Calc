28f980a7d12b4937265386be210c5c4f48ec5c12
fefragoso <felipeofragoso@gmail.com>
2022-08-10 12:34:17 -0300

    Adcionando suporte a equações de 1º grau

--- script.js
+++ script.js
@@ -8,8 +8,25 @@ function calcular() {
     let B = valorB.value
     let C = valorC.value
 
-    let resultadoMaior = (-B + Math.sqrt(B**2-4*A*C))/(2*A)
-    let resultadoMenor = (-B - Math.sqrt(B**2-4*A*C))/(2*A)
+    if (A == 0) {
+        C = C*-1
 
-    alert(resultadoMaior + ' e ' + resultadoMenor)
+        alert("Como o valor de 'A' foi 0, a função foi de 1º grau.\nValor de X: " + C/B)
+    }
+    else {
+
+        let delta = B**2-4*A*C
+
+        if (delta < 0) {
+
+            alert("O resultado não pode ser encontrado, pois o valor de ∆ é uma raiz negativa.")
+        }
+        else {
+
+            let resultadoMaior = (-B + Math.sqrt(delta))/(2*A)
+            let resultadoMenor = (-B - Math.sqrt(delta))/(2*A)
+        
+            alert(resultadoMaior + ' e ' + resultadoMenor)
+        }
+    }
 }
